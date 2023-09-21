227f5c0a3207598c2844648055a0bb15c9aa6bbb
salma <gsalmahossain@gmail.com>
2023-09-21 19:46:42 +0200

    clean and improved myMath test

--- src/info/salma/test/MyMathTest.java
+++ src/info/salma/test/MyMathTest.java
@@ -7,15 +7,18 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyMathTest {
+   private MyMath math = new MyMath();
 
     @Test
-    void test() {
-        int[] numbers = {1,2,3};
-        MyMath math = new MyMath();
-        int result = math.calculateSum(numbers);
-        System.out.println(result);
-        int expectedResult = 6;
-        assertEquals(expectedResult, result);
-
+    void calculateSum_threeMemberArray() {
+//        int[] numbers = {1,2,3};
+//        int result = math.calculateSum(numbers);
+//        int expectedResult = 6;
+//        assertEquals(expectedResult, result);
+        assertEquals(6, math.calculateSum(new int[]{1,2,3}));
+    }
+    @Test
+    void calculateSum_ZeroLengthArray() {
+        assertEquals(0, math.calculateSum(new int[]{}));
     }
 }
